53bd96a00390c8bf862132275fd4518de1223bbf
misbiheyv <mishagorkovenko1103@gmail.com>
2022-02-10 13:18:52 +1000

    add cors support

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ import express from "express";
 import DBQueries from "./DB/DBQueries.js";
 
 const app = express()
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Expose-Headers', 'Content-Disposition')
+    next()
+}
+app.use(allowCrossDomain)
 const PORT = process.env.PORT || 5001
 
 app.get('/save_card', (req, res) => {
